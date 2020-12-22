71c4cb7d6651816cd9e7fc73a74f3021f247ecec
Kolbjørn Halle <kolhal92@hotmail.com>
2020-12-22 21:05:40 +0100

    create blazor app frontend , register user with from blazor app with API

--- BookStore-UI/Models/UserModel.cs
+++ BookStore-UI/Models/UserModel.cs
@@ -4,17 +4,30 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace bookStore_API3.DTOs
+namespace BookStore_UI.Models
 {
-    public class UserDTO
+    public class RegistrationModel
     {
+
         [Required]
         [EmailAddress]
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
+
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        
+        public string ConfirmPassword { get; set; }
+    }
+    public class LoginModel
+    {
     }
 }
